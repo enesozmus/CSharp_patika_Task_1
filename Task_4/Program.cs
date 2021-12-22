8fa589e274f27c9725f57a759776d9255b1108cc
enesozmus <ensozmus@gmail.com>
2021-12-22 22:49:22 +0300

    firstCommit

---
+++ Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle yazınız: ");
+            string sentence = Console.ReadLine();
+            string letters_list = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ" + "abcçdefgğhiıjklmnoöprsştuüvyz";
+            string[] words = sentence.Split(' ');
+            Console.WriteLine($"Yazdığınız cümledeki kelime adedi: {words.Length}");
+
+            int sayac = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            if (letters_list.Contains(sentence[i]))
+            sayac++;
+
+            Console.WriteLine($"Yazdığınız cümledeki harf adedi: {sayac}");
+        }
+    }
+}
